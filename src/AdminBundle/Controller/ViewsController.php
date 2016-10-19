f255b7a88d6edeaa957b5cdfe959fb89c4d1961d
maddyzi <maddyx@hotmail.fr>
2016-10-19 12:30:01 +0200

    creation admin bundle ainsi que les controllers, et enfin les views

---
+++ src/AdminBundle/Controller/ViewsController.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace AdminBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Description of ViewsController
+ *
+ * @author maddyson
+ */
+class ViewsController extends Controller {
+
+    /**
+     * @Route("/admin/accueil")
+     * @Template("AdminBundle::accueil.html.twig")
+     */
+    public function accueil() {
+     
+    }
+    
+    /**
+     * @Route("/admin/categories")
+     * @Template("AdminBundle::categories.html.twig")
+     */
+    public function categories() {
+        
+    }
+    
+    /**
+     * @Route("/admin/detailsArticles")
+     * @Template("AdminBundle::detailsArticles.html.twig")
+     */
+    public function detailsArticles() {
+        
+    }
+    
+    /**
+     * @Route("/admin/profil")
+     * @Template("AdminBundle::profil.html.twig")
+     */
+    public function profil() {
+        
+    }
+
+}
